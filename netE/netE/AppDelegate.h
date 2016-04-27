1f28dd91dd42be027b17d0f0680f6aa0771782eb
cappuccinoNuts <767695089@qq.com>
2016-04-28 03:58:20 +0800

    初始化项目

---
+++ netE/netE/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  netE
+//
+//  Created by mypc on 16/4/28.
+//  Copyright © 2016年 xiaokaTechCo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ netE/netE/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  netE
+//
+//  Created by mypc on 16/4/28.
+//  Copyright © 2016年 xiaokaTechCo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
